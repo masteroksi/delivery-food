e2cc97da69797e831e1f285ff2f7bd3049091544
masteroksi <masteroksi@gmail.com>
2020-05-06 19:56:55 +0300

    add card

--- js/main.js
+++ js/main.js
@@ -28,7 +28,6 @@ logInForm.addEventListener('submit', handleLogin);
 function handleLogin(ev) {
     ev.preventDefault();
     const data = new FormData(ev.target);
-    // dataObj = {login: value, password: value}
     const dataObj = Object.fromEntries(data.entries());
 
     const login = document.getElementById('login');
@@ -63,3 +62,98 @@ function clearLoginForm() {
 }
 
 // auth end
+
+// cards
+const cardsRestaurants = document.querySelector('.cards-restaurants');
+const containerPromo = document.querySelector('.container-promo');
+const restaurants = document.querySelector('.restaurants');
+const menu = document.queryCommandEnabled('.menu');
+const logo = document.querySelector('.logo');
+const cardsMenu = document.querySelector('.cards-menu');
+
+function createCardsRestaurants(restoranItem) {
+    const card = `<a href="${restoranItem.url}" class="card card-restaurant">
+						<img src="img/pizza-plus/preview.jpg" alt="image" class="card-image" />
+						<div class="card-text">
+							<div class="card-heading">
+								<h3 class="card-title">Пицца плюс </h3>
+								<span class="card-tag tag">50 мин </span>
+							</div>
+							<div class="card-info">
+								<div class="rating">
+									4.5 
+								</div>
+								<div class="price">От 900 ₽ </div>
+								<div class="category">Пицца </div>
+							</div>
+						</div>
+						</a>
+`;
+
+    cardsRestaurants.insertAdjacentHTML('afterbegin', card);
+}
+
+
+
+function createCardGood() {
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.insertAdjacentHTML('beforeend', `
+					<div class="card">
+						<img src="img/pizza-plus/pizza-girls.jpg" alt="image" class="card-image"/>
+						<div class="card-text">
+							<div class="card-heading">
+								<h3 class="card-title card-title-reg">Пицца Девичник</h3>
+							</div>
+							<div class="card-info">
+								<div class="ingredients">Соус томатный, постное тесто, нежирный сыр, кукуруза, лук, маслины,
+									грибы, помидоры, болгарский перец.
+								</div>
+							</div>
+								<button class="button button-primary button-add-cart">
+									<span class="button-card-text">В корзину</span>
+									<span class="button-cart-svg"></span>
+								</button>
+								<strong class="card-price-bold">450 ₽</strong>
+							</div>
+						</div>
+					</div>
+        `);
+    cardsMenu.insertAdjacentElement('beforeend', card);
+
+}
+
+function openGoods(event) {
+    const target = event.target;
+    // const restaurant = target.parentElement;
+    const restaurant = target.closest('.cards-restaurants');
+
+    if (restaurant) {
+        containerPromo.classList.add('hide');
+        restaurants.classList.add('hide');
+        menu.classList.remove('hide');
+
+        cardsMenu.textContent = '';
+
+        createCardGood();
+        createCardGood();
+        createCardGood();
+        createCardGood();
+
+    }
+
+
+}
+
+cardsRestaurants.addEventListener('click', openGoods);
+
+logo.addEventListener('click', function () {
+    containerPromo.classList.remove('hide');
+    restaurants.classList.remove('hide');
+    menu.classList.add('hide');
+});
+
+
+
+
+// cards end
